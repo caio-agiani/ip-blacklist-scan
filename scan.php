fcf7f283d51396558cac5df05b5565cce1c78626
Caio Agiani <42050888+caio-agiani@users.noreply.github.com>
2019-03-04 08:16:36 -0300

    Add files via upload
    Api to https://apility.io
    
    Use ?ip=198.46.178.97

---
+++ scan.php
@@ -0,0 +1,88 @@
+<?php
+
+/** 
+ * Author: Caio Agiani
+ * Description: IP Blacklist abuse scan
+ * Website: https://apility.io/
+ */
+
+extract($_GET);
+
+if (isset($_GET['ip'])) {
+
+    class iPScan {
+
+        public function _cURL($url, $post = false, $header = array('')) {
+    
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+            if ($post)
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+            return $data;
+        }
+    }
+
+    function color ($hex, $text) {
+        return '<font class="label label-danger" style="background-color:' .$hex. '">' .$text. '</font>';
+    }
+
+    if (filter_var($ip, FILTER_VALIDATE_IP)) {
+        $new = new iPScan;
+        $obj = json_decode($new->_cURL('https://api.apility.net/v2.0/ip/'. $ip .'?items=100', false, array('Content-Type: application/json','X-Auth-Token: 8f307bb1-a933-4dcb-b7c7-6664f5519d3a')), true);
+
+        $j = $obj['fullip']['badip'];
+        $score = $j['score'];
+
+        if ($score !== 0) {
+            $msg = '';
+
+            foreach ($j as $key => $value) {
+                $num = is_array($value) ? count($value) : 0;
+            }
+            
+            foreach ($value as $key => $value) {
+                $msg .= ' '. color('#FF0000', $value);
+            }
+
+            echo $ip. ' WAS FOUND IN <b>' .$num. '</b> BLACK LIST(S): '. $msg;
+        }
+        else {
+            echo $ip. ' ' .color('#00FF00', 'NOT FOUND'). ' BLACK LIST(S)';
+        }
+
+        if ($obj['fullip']['history']['score'] !== 0) {
+            $j = $obj['fullip']['history']['activity'];
+
+            echo "<hr />IP address historic information: <br />";
+
+            foreach ($j as $key => $value) {
+                echo "<pre />";
+
+                $cmd = $value['command'];
+                $blacklist = $value['blacklists'] ? $value['blacklists'] : $value['blacklist_change'];
+
+                $final = str_replace('add', '+ '.color('#FF0000', 'IP was added to blacklist(s)'), $cmd. ' -> ['. $blacklist. ']');
+                echo $final = str_replace('rem', '- '.color('#00FF00', 'IP was removed from blacklist(s)'), $final);
+            }
+        }
+        else {
+            echo '<hr />IP address historic information: '. color('#00FF00', '0');
+        }
+    } 
+    else {
+        echo 'IP inválido';
+    }
+}
+else {
+    echo 'Use ?ip=198.46.178.97';
+}
